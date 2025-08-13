6412a74bba4435ea65d4039eed30a27843dd9d16
Haruto Taketomi <takebamboo88@gmail.com>
2025-08-13 17:09:14 +0900

    不必要なファイルを除いた再push

--- MMJ_GSsim/obj/x86/Debug/net8.0-windows10.0.19041.0/win-x86/MMJ_GSsim.AssemblyInfo.cs
+++ MMJ_GSsim/obj/x86/Debug/net8.0-windows10.0.19041.0/win-x86/MMJ_GSsim.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MMJ_GSsim")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53639b3fbc4d6b8df9546aa084193cb2ecf04553")]
 [assembly: System.Reflection.AssemblyProductAttribute("MMJ_GSsim")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MMJ_GSsim")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
